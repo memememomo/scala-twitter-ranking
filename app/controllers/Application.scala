237cddeba5ef0ecd61270f2c69d89fa572528d94
Uchiko <memememomo@gmail.com>
2016-03-05 10:54:59 +0900

    Implement form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,15 +4,32 @@ import org.joda.time.DateTime
 import play.api._
 import play.api.mvc._
 import models.TwitterApi
+import play.api.data._
+import play.api.data.Forms._
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+
+case class QueryData(keyword: String, since: DateTime)
 
 object Application extends Controller {
 
-  def index = Action {
-    val ranking = TwitterApi.ranking(
-      new DateTime(),
-      ""
-    )
-    Ok(views.html.index("Your new application is ready."))
+  val queryForm = Form(
+    mapping(
+      "keyword" -> text,
+      "since" -> jodaDate
+    )(QueryData.apply)(QueryData.unapply)
+  )
+
+  def index = Action { implicit req =>
+    Ok(views.html.index(queryForm, None))
   }
 
+  def search = Action { implicit req =>
+    val queryData = queryForm.bindFromRequest.get
+    val ranking = TwitterApi.ranking(
+      queryData.since,
+      queryData.keyword
+    )
+    Ok(views.html.index(queryForm, Some(ranking)))
+  }
 }
