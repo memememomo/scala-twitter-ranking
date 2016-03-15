64902cf9a9abe0a6a5c12295e6dea1e0c97d07c0
Uchiko <memememomo@gmail.com>
2016-03-16 02:25:35 +0900

    Impliment storage

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,5 @@
 package controllers
 
-import java.io.{ObjectInputStream, ByteArrayInputStream, ObjectOutputStream, ByteArrayOutputStream}
-
-import akka.serialization.Serialization
-import org.apache.commons.lang3
-import org.apache.commons.lang3.SerializationUtils
 import org.joda.time.DateTime
 import play.api._
 import play.api.libs.json.{JsPath, Json}
@@ -15,10 +10,6 @@ import play.api.data.Forms._
 import javax.inject.Inject
 import play.api.i18n.{MessagesApi, I18nSupport}
 import com.redis._
-import serialization._
-import Parse.Implicits.parseByteArray
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
 
 case class QueryData(keyword: String, since: DateTime)
 case class KeywordData(keyword: String)
@@ -39,13 +30,8 @@ class Application @Inject() (implicit val messagesApi: MessagesApi, webJarAssets
     )(KeywordData.apply)(KeywordData.unapply)
   )
 
-  val cacheSeconds = 30L * 60
-
-
-
   def index = Action { implicit req =>
-    val redisClient = new RedisClient("localhost", 6379)
-    val form = redisClient.get[String]("keyword") match {
+    val form = Storage.getKeyword match {
       case Some(k) => queryForm.fill(QueryData(k, DateTime.now))
       case _ => queryForm
     }
@@ -53,39 +39,17 @@ class Application @Inject() (implicit val messagesApi: MessagesApi, webJarAssets
   }
 
   def search = Action { implicit req =>
-    implicit val rankingReads: Reads[Ranking] = (
-      (JsPath \ "rank").read[Int] and
-      (JsPath \ "name").read[String] and
-      (JsPath \ "text").read[String] and
-      (JsPath \ "favorite").read[Int] and
-      (JsPath \ "retweet").read[Int] and
-      (JsPath \ "imageUrls").read[List[String]]
-    )(Ranking.apply _)
-
-    implicit val rankingWrites: Writes[Ranking] = (
-      (JsPath \ "rank").write[Int] and
-      (JsPath \ "name").write[String] and
-      (JsPath \ "text").write[String] and
-      (JsPath \ "favorite").write[Int] and
-      (JsPath \ "retweet").write[Int] and
-      (JsPath \ "imageUrls").write[List[String]]
-    )(unlift(Ranking.unapply))
-
     val queryData = queryForm.bindFromRequest.get
-    val redisClient = new RedisClient("localhost", 6379)
-    redisClient.get(queryData.toString) match {
+    Storage.getRanking(queryData.toString) match {
       case Some(r) => {
-        Logger.debug("Hit cache. ttl: " + redisClient.ttl(queryData.toString))
-        val json = Json.parse(r)
-        val ranking = json.validate[List[Ranking]]
-        Ok(views.html.index(queryForm.fill(queryData), ranking.asOpt))
+        Ok(views.html.index(queryForm.fill(queryData), Some(r)))
       }
       case None =>
         val ranking = TwitterApi.ranking(
           queryData.since,
           queryData.keyword
         )
-        redisClient.set(queryData.toString, Json.toJson(ranking).toString, false, Seconds(cacheSeconds))
+        Storage.setRanking(queryData.toString, ranking)
         Ok(views.html.index(queryForm.fill(queryData), Some(ranking)))
     }
   }
@@ -96,8 +60,7 @@ class Application @Inject() (implicit val messagesApi: MessagesApi, webJarAssets
 
   def createKeyword = Action { implicit req =>
     val keywordData = keywordForm.bindFromRequest.get
-    val redisClient = new RedisClient("localhost", 6379)
-    redisClient.set("keyword", keywordData.keyword)
+    Storage.setKeyword(keywordData.keyword)
     Logger.debug("Create keyword: " + keywordData.keyword)
     Redirect(routes.Application.keyword)
   }
---
+++ app/models/Storage.scala
@@ -0,0 +1,52 @@
+package models
+
+import com.redis.RedisClient
+import play.api.Logger
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+object Storage {
+  val keywordCacheName = "keyword"
+  val redisClient = new RedisClient("localhost", 6379)
+
+  implicit val rankingReads: Reads[Ranking] = (
+    (JsPath \ "rank").read[Int] and
+    (JsPath \ "name").read[String] and
+    (JsPath \ "text").read[String] and
+    (JsPath \ "favorite").read[Int] and
+    (JsPath \ "retweet").read[Int] and
+    (JsPath \ "imageUrls").read[List[String]]
+  )(Ranking.apply _)
+
+  implicit val rankingWrites: Writes[Ranking] = (
+    (JsPath \ "rank").write[Int] and
+    (JsPath \ "name").write[String] and
+    (JsPath \ "text").write[String] and
+    (JsPath \ "favorite").write[Int] and
+    (JsPath \ "retweet").write[Int] and
+    (JsPath \ "imageUrls").write[List[String]]
+  )(unlift(Ranking.unapply))
+
+  def getKeyword = {
+    redisClient.get[String](keywordCacheName)
+  }
+
+  def setKeyword(keyword: String) = {
+    redisClient.set(keywordCacheName, keyword)
+  }
+
+  def getRanking(key: String) = {
+    redisClient.get(key) match {
+      case Some(r) => {
+        Logger.debug("Hit cache.")
+        val json = Json.parse(r)
+        json.validate[List[Ranking]].asOpt
+      }
+      case _ => None
+    }
+  }
+
+  def setRanking(key: String, ranking: List[Ranking]) = {
+    redisClient.set(key, Json.toJson(ranking).toString)
+  }
+}
